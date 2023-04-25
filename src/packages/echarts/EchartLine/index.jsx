ee3b8179f0f097f912a2d5327cb1a1982214e46c
jixiaojiao <cc_byland@163.com>
2023-04-25 19:40:24 +0800

    1

--- src/packages/echarts/EchartLine/index.jsx
+++ src/packages/echarts/EchartLine/index.jsx
@@ -1,8 +1,10 @@
 import { defineComponent } from "vue";
 import core from "../core";
+import main from './main'
 
 const component = Object.assign({}, core, {
   name: "echart-line",
+  mixins: [ main ]
 });
 
 export default defineComponent(component);

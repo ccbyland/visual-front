140cf751cf052daab27df6882ee043be9a4957df
jixiaojiao <cc_byland@163.com>
2023-05-18 09:39:18 +0800

    1

--- src/components/editor-main/index.jsx
+++ src/components/editor-main/index.jsx
@@ -26,15 +26,22 @@ export default defineComponent({
     });
 
     const widgetConfig = inject("widgetConfig");
+
+    const { commands } = useCommand(data);
+    const { mousedownCanvas, mousedownCanvasWidget } = useFocus(data);
+
+    const canvasRef = ref(null);
+    const gridLayoutRef = ref(null);
+    const gridItemRef = ref([]);
+    const forbidChildPointerEvents = ref(false)
+
+    const setGridItemRef = (item) => {
+      gridItemRef.value.push(item);
+    };
+
     const gridItemOperated = () => {
       events.emit("dragend");
     };
-    const { mousedownCanvas, mousedownCanvasWidget } = useFocus(data);
-
-    const gridItemRef = ref([]);
-    const gridLayoutRef = ref(null);
-    const canvasRef = ref(null);
-    const { commands } = useCommand(data);
 
     const addWidget = (widget) => {
       const defaultData = widget.defaultData;
@@ -53,11 +60,12 @@ export default defineComponent({
       commands.addWidget(newWidget);
     };
 
-    const { dragStartWidget, dragEndWidget } = useWidgetDragger(
+    const { dragstartWidget, dragendWidget } = useWidgetDragger(
       canvasRef,
       data,
+      gridLayoutRef,
       gridItemRef,
-      gridLayoutRef
+      forbidChildPointerEvents
     );
 
     return () => {
@@ -68,69 +76,80 @@ export default defineComponent({
       const globalStyle = props.modelValue.container.props;
       const gridMarginArr = GlobalStyle.getGridMarginArr(containerData);
 
-      return (
-        <div className="editor-main">
-          <div className="editor-tool">
-            <div className="item">
-              <div className="label">图表</div>
-              <div class="options">
-                {widgetConfig.widgetList.map((widget) => {
-                  return (
-                    <div
-                      draggable
-                      onDragStart={(e) => dragStartWidget(e, widget)}
-                      onDragEnd={dragEndWidget}
-                      className="node"
-                      onClick={() => addWidget(widget)}
-                    >
-                      <img
-                        className="icon"
-                        src={require(`@/assets/images/canvas-module/${widget.key}.svg`)}
-                      />
-                    </div>
-                  );
-                })}
-              </div>
-            </div>
-          </div>
-          <div
-            className="editor-canvas"
-            style={pageStyle}
-            onMousedown={mousedownCanvas}
-          >
-            <grid-layout
-              v-model:layout={data.value.widgets}
-              col-num={12}
-              row-height={20}
-              is-draggable={true} // 是否可拖拽
-              is-resizable={true} // 是否可设置大小
-              vertical-compact={true} // 是否垂直紧凑布局
-              margin={gridMarginArr}
-              use-css-transforms={true}
-            >
-              {data.value.widgets.map((widget, widgetIndex) => {
+      const editorTool = (
+        <div className="editor-tool">
+          <div className="item">
+            <div className="label">图表</div>
+            <div class="options">
+              {widgetConfig.widgetList.map((widget) => {
                 return (
-                  <grid-item
-                    id={`grid-item__${widget.i}`}
-                    x={widget.x}
-                    y={widget.y}
-                    w={widget.w}
-                    h={widget.h}
-                    i={widget.i}
-                    onMoved={gridItemOperated}
-                    onMousedown={(e) =>
-                      mousedownCanvasWidget(e, widget, widgetIndex)
-                    }
+                  <div
+                    class="node"
+                    draggable
+                    onDragstart={(e) => dragstartWidget(e, widget)}
+                    onDragend={dragendWidget}
+                    onClick={() => addWidget(widget)}
                   >
-                    <EditorWidget
-                      v-model:widget={widget}
-                      globalStyle={globalStyle}
-                    ></EditorWidget>
-                  </grid-item>
+                    <img
+                      className="icon"
+                      src={require(`@/assets/images/canvas-module/${widget.key}.svg`)}
+                    />
+                  </div>
                 );
               })}
-            </grid-layout>
+            </div>
           </div>
+        </div>
+      );
+
+      const editorCanvas = (
+        <div
+          id="canvas_content"
+          ref={canvasRef}
+          className="editor-canvas"
+          style={pageStyle}
+          onMousedown={mousedownCanvas}
+        >
+          <grid-layout
+            ref={gridLayoutRef}
+            v-model:layout={data.value.widgets}
+            col-num={12}
+            row-height={20}
+            is-draggable={true} // 是否可拖拽
+            is-resizable={true} // 是否可设置大小
+            vertical-compact={false} // 是否垂直紧凑布局
+            margin={gridMarginArr}
+            use-css-transforms={true}
+          >
+            {data.value.widgets.map((widget, widgetIndex) => {
+              return (
+                <grid-item
+                  ref={setGridItemRef}
+                  id={`grid-item__${widget.i}`}
+                  x={widget.x}
+                  y={widget.y}
+                  w={widget.w}
+                  h={widget.h}
+                  i={widget.i}
+                  onMoved={gridItemOperated}
+                  onMousedown={(e) =>
+                    mousedownCanvasWidget(e, widget, widgetIndex)
+                  }
+                >
+                  <EditorWidget
+                    v-model:widget={widget}
+                    globalStyle={globalStyle}
+                  ></EditorWidget>
+                </grid-item>
+              );
+            })}
+          </grid-layout>
+        </div>
+      );
+      return (
+        <div className="editor-main">
+          {editorTool}
+          {editorCanvas}
         </div>
       );
     };
--- src/hooks/useWidgetDragger.js
+++ src/hooks/useWidgetDragger.js
@@ -1,43 +1,157 @@
-export default (canvasRef) => {
+import { randomStr } from "@/utils";
+import useDefaultData from "./useDefaultData";
 
-    let curWidget = null
+export default (
+  canvasRef,
+  data,
+  gridLayoutRef,
+  gridItemRef,
+  forbidChildPointerEvents
+) => {
+  let curWidget = null;
+  let mouseXY = { x: null, y: null };
+  let parentRect = {};
+  let newPos = {};
+  let dragPos = {};
 
-    const dragenter = (e) => {
-        e.dataTransfer.dropEffect = 'move'
+  let w = 0;
+  let h = 0;
+  let id = 0;
+  let defaultData = {};
+  let props = {};
+  let query = {};
+  //   const dragenter = (e) => {
+  //     console.error("dragenter");
+  //     e.dataTransfer.dropEffect = "link";
+  //   };
+
+  const dragover = (e) => {
+    console.error("dragover");
+    e.dataTransfer.dropEffect = "move";
+    e.preventDefault();
+    // 返回元素的大小及其相对于视口的位置
+    parentRect = document
+      .getElementById("canvas_content")
+      .getBoundingClientRect();
+    // 存储当前鼠标位置
+    mouseXY.x = e.clientX;
+    mouseXY.y = e.clientY;
+    // 获取当前小部件列表
+    let widgets = data.value.widgets;
+    // 查找尚未初始化的小部件
+    let index = widgets.findIndex((item) => item.i === "");
+
+    // 当前不存在未初始化的小部件
+    if (index === -1) {
+      data.value = {
+        ...data.value,
+        widgets: [
+          ...widgets,
+          {
+            i: "",
+            x: 0,
+            y: 0,
+            w,
+            h,
+            props,
+            query,
+          },
+        ],
+      };
+
+      // 当前存在未初始化的小部件
+    } else {
+      // 获取该小部件
+      let el = gridItemRef.value[index];
+
+      /**
+       * 将x和y坐标从像素转换为栅格单位。
+       * @param｛Number｝top顶部位置（相对于父级）（以像素为单位）。
+       * @param｛Number｝left左位置（相对于父级），以像素为单位。
+       * @return｛Object｝x和y，以网格为单位。
+       */
+      newPos = el.calcXY(
+        mouseXY.y - parentRect.top,
+        mouseXY.x - parentRect.left
+      );
+
+      // 移动目标位置
+      gridLayoutRef.value.dragEvent("dragstart", "", newPos.x, newPos.y, h, w);
+
+      dragPos.x = newPos.x;
+      dragPos.y = newPos.y;
     }
+  };
 
-    // const dragover = (e) => {
+  const dragleave = (e) => {
+    e.preventDefault();
+    console.error("dragleave");
 
-    // }
+    gridLayoutRef.value.dragEvent("dragend", "", newPos.x, newPos.y, h, w);
+    let widgets = data.value.widgets.filter((obj) => obj.i !== "");
+    data.value = { ...data.value, widgets: [...widgets] };
+  };
 
-    // const dragleave = (e) => {
+  const drop = (e) => {
+    console.error("drop");
 
-    // }
+    gridLayoutRef.value.dragEvent("dragend", "", dragPos.x, dragPos.y, h, w);
+    let widgets = data.value.widgets.filter((obj) => obj.i !== "");
+    data.value = {
+      ...data.value,
+      widgets: [
+        ...widgets,
+        {
+          i: id,
+          x: 0,
+          y: 0,
+          w,
+          h,
+          key: curWidget.key,
+          props,
+          query,
+        },
+      ],
+    };
+  };
 
-    // const drop = (e) => {
+  const checkWidgetAttr = (widget) => {
+    forbidChildPointerEvents.value = true;
+    defaultData = widget.defaultData;
+    const { getProps, getQuery } = useDefaultData(widget);
+    props = getProps("styles");
+    query = getQuery();
+    id = randomStr(8);
+    w = defaultData.layout.w;
+    h = defaultData.layout.h;
+  };
 
-    // }
+  const dragstartWidget = (e, widget) => {
+    forbidChildPointerEvents.value = false;
+    console.error("dragstartWidget");
+    // canvasRef.value.addEventListener("dragenter", dragenter);
+    // 当在画布区移动时，添加草稿部件，并跟随鼠标位置异动
+    canvasRef.value.addEventListener("dragover", dragover);
+    // 离开画布区时，清空目标拖拽数据，避免产生半成品
+    // canvasRef.value.addEventListener("dragleave", dragleave);
+    // 当在画布区释放时，添加对应小部件
+    canvasRef.value.addEventListener("drop", drop);
+    curWidget = widget;
 
-    const dragstartWidget = (e, widget) => {
+    checkWidgetAttr(widget);
+  };
 
-        canvasRef.value.addEventListener('dragenter', dragenter)
-        // canvasRef.value.addEventListener('dragover', dragover)
-        // canvasRef.value.addEventListener('dragleave', dragleave)
-        // canvasRef.value.addEventListener('drop', drop)
-        curWidget = widget
-    }
+  const dragendWidget = () => {
+    console.error("dragendWidget");
+    // canvasRef.value.removeEventListener("dragenter", dragenter);
+    canvasRef.value.removeEventListener("dragover", dragover);
+    // canvasRef.value.removeEventListener("dragleave", dragleave);
+    canvasRef.value.removeEventListener("drop", drop);
+    curWidget = null;
+  };
 
-    const dragendWidget = () => {
-
-        canvasRef.value.removeEventListener('dragenter', dragenter)
-        // canvasRef.value.removeEventListener('dragover', dragover)
-        // canvasRef.value.removeEventListener('dragleave', dragleave)
-        // canvasRef.value.removeEventListener('drop', drop)
-        curWidget = null
-    }
-
-    return {
-        dragstartWidget,
-        dragendWidget
-    }
-}
+  return {
+    dragstartWidget,
+    dragendWidget,
+  };
+};
